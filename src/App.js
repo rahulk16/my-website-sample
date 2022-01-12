e2f20abd31ad1ba0e899046b25f07ea9ec9deb8c
rahulk16 <rahul162rk@gmail.com>
2022-01-13 02:27:14 +0530

    UI started with React.JS

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          Coming Soon
+      </header>
+    </div>
+  );
+}
+
+export default App;
